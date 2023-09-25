6590dd47eec7e956950d56ab0e280f15a5802003
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-09-25 08:47:49 +0000

    UPDATE TOUCHOSC SCRIPT TO START THE BRIDGE AS WELL

--- scripts/start-touchosc.ps1
+++ scripts/start-touchosc.ps1
@@ -1,4 +1,16 @@
-touchosc `
-  --general.ui.editor false `
-  --general.ui.fullscreen true `
-  src/touchosc/scamp.tosc
+function Start-TouchOscProcesses {
+  Write-Host 'Starting TouchOSC Bridge…' 
+
+  Start-Process `
+    -WorkingDirectory 'C:\Program Files (x86)\TouchOSC Bridge' `
+    'TouchOSC Bridge.exe'
+
+  Write-Host 'Starting TouchOSC in fullscreen…' 
+
+  touchosc `
+    --general.ui.editor false `
+    --general.ui.fullscreen true `
+    'src/touchosc/scamp.tosc'
+}
+
+Start-TouchOscProcesses
