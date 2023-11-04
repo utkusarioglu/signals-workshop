4fdb9efc0e477331bbb9991087740668b694a784
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-11-04 07:38:54 +0000

    OSC SERVER WORKS WITH CUBASE NOW

--- src/osc_server/server.py
+++ src/osc_server/server.py
@@ -4,15 +4,20 @@ from time import sleep
 
 
 app = Flask(__name__)
-client = udp_client.SimpleUDPClient("192.168.1.151", 57125)
+client = udp_client.SimpleUDPClient("192.168.1.151", 57120)
 
 
 @app.route("/")
 def hello_world():
-    return "<button onclick=\"fetch('http://192.168.1.151:5000/osc')\">play</button>"
+    variants = sorted([0, 10, 50, 60, 70, 90, 100, 127])
+    buttons = [
+        f'<button style="height: 200px; width: 100px;" onclick="fetch(\'http://192.168.1.151:5000/osc/{elem}\')">{elem}</button>'
+        for elem in variants
+    ]
+    return "".join(buttons)
 
 
-@app.route("/osc")
-def osc():
-    client.send_message("/aa", 13)
+@app.route("/osc/<int:value>")
+def osc(value):
+    client.send_message("/cc", value)
     return {"message": "received"}
