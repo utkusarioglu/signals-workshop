41298086cd37df8eb0f97829951da0ce0aca39b5
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-11-05 09:08:17 +0000

    OSC THROUGH WEBSOCKETS

--- src/osc_server/server.py
+++ src/osc_server/server.py
@@ -1,23 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from pythonosc import udp_client
-from time import sleep
-
+from flask_sock import Sock
 
 app = Flask(__name__)
+sock = Sock(app)
 client = udp_client.SimpleUDPClient("192.168.1.151", 57120)
 
 
 @app.route("/")
-def hello_world():
-    variants = sorted([0, 10, 50, 60, 70, 90, 100, 127])
-    buttons = [
-        f'<button style="height: 200px; width: 100px;" onclick="fetch(\'http://192.168.1.151:5000/osc/{elem}\')">{elem}</button>'
-        for elem in variants
-    ]
-    return "".join(buttons)
+def root():
+    return render_template("index.html")
 
 
-@app.route("/osc/<int:value>")
-def osc(value):
-    client.send_message("/cc", value)
-    return {"message": "received"}
+@sock.route("/ws")
+def ws_supercollider(socket):
+    while True:
+        value = socket.receive()
+        client.send_message("/cc", value)
