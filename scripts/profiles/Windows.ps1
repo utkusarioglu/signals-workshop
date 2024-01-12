0e767d4d6baadfd8cad3fe3cb9f599f26af25df0
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-01-12 07:02:26 +0000

    Chore: Rename assets

--- scripts/profiles/Windows.ps1
+++ scripts/profiles/Windows.ps1
@@ -1,5 +1,2 @@
-# Import-Module 'scripts\profiles\SuperCollider.psm1'
 Import-Module $PSScriptRoot\SuperCollider.psm1
 Import-Module $PSScriptRoot\OscServer.psm1
-
-Write-Host 'Injected profile functions.'
